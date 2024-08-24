3708c5009d7923de3898481804e196d77a2f80b4
itsab <itsab@ABHISHEK.mshome.net>
2024-08-24 13:49:50 +0530

    Product , added new POM class

---
+++ com.crm.vtiger1234/src/main/java/com/comcast/crm/objectrepositoryutility/ProductsPage.java
@@ -0,0 +1,14 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductsPage {
+	@FindBy(xpath="//img[@alt='Create Product...']")
+	private WebElement createproductimgBtn;
+
+	public WebElement getCreateproductimgBtn() {
+		return createproductimgBtn;
+	}
+
+}
