98c8fc736e3e34e74c1fe858e8428175f946e4d9
itsab <itsab@ABHISHEK>
2024-08-27 10:53:17 +0530

    product POM class
    added new element 2

--- com.crm.vtiger1234/src/main/java/com/comcast/crm/objectrepositoryutility/ProductsPage.java
+++ com.crm.vtiger1234/src/main/java/com/comcast/crm/objectrepositoryutility/ProductsPage.java
@@ -6,6 +6,13 @@ import org.openqa.selenium.support.FindBy;
 public class ProductsPage {
 	@FindBy(xpath="//img[@alt='Create Product...']")
 	private WebElement createproductimgBtn;
+	
+	@FindBy(name="searchBtn")
+	private WebElement ele1;
+
+	public WebElement getEle1() {
+		return ele1;
+	}
 
 	public WebElement getCreateproductimgBtn() {
 		return createproductimgBtn;
